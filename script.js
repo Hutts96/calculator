a0835fa9017bddedef6a7cb33cccb6afaf6a8df3
Hutts96 <84289781+Hutts96@users.noreply.github.com>
2023-09-21 21:28:36 +0300

    Add functionality to C and Del buttons

--- script.js
+++ script.js
@@ -26,7 +26,39 @@ function operate(a, operand, b) {
     };
 }
 
+function displayPressedKeys(toAdd) {
+    displayValue = displayValue * 10 + toAdd;
+    screen.textContent = displayValue;
+}
+
+function updateValues(newVal) {
+    displayValue = newVal;
+    screen.textContent = displayValue;
+}
+
+
+
+const screen = document.querySelector('.screen');
+const numbers = document.querySelectorAll('.number');
+const clear = document.querySelector('.clear');
+const del = document.querySelector('.del');
+
 let a;
 let operand;
 let b;
+let displayValue = 0;
+
+screen.textContent = '0';
+
+numbers.forEach((number) => {
+    number.addEventListener('click', (event) => {
+        if (displayValue.toString().length > 15) return;
+        displayPressedKeys(+event.target.textContent);
+    })
+});
+clear.addEventListener('click', () => { displayValue = 0; screen.textContent = '0'; });
+del.addEventListener('click', () => { updateValues(Math.floor(displayValue / 10)); });
+window.addEventListener('keydown', (e) => {
+    if (e.key = 'Backspace') updateValues(Math.floor(displayValue / 10));
+}); //gonna need to to something like this for all keys
 
