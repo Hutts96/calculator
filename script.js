fd6a5124ce7c4962341aa037aba6a5e27618b262
Hutts96 <84289781+Hutts96@users.noreply.github.com>
2023-09-22 20:55:04 +0300

    Add functionality to operators including equal

--- script.js
+++ script.js
@@ -28,7 +28,7 @@ function operate(a, operand, b) {
 
 function displayPressedKeys(toAdd) {
     displayValue = displayValue * 10 + toAdd;
-    screen.textContent = displayValue;
+    screen.textContent += toAdd;
 }
 
 function updateValues(newVal) {
@@ -36,19 +36,27 @@ function updateValues(newVal) {
     screen.textContent = displayValue;
 }
 
-
+function clear() {
+    lValue = 0;
+    rValue = 0;
+    operator = '';
+    displayValue = 0;
+    screen.textContent = '';
+}
 
 const screen = document.querySelector('.screen');
 const numbers = document.querySelectorAll('.number');
-const clear = document.querySelector('.clear');
+const cl = document.querySelector('.clear');
 const del = document.querySelector('.del');
+const operators = document.querySelectorAll('.operator');
+const equals = document.querySelector('.equals');
 
-let a;
-let operand;
-let b;
+let lValue;
+let operator;
+let rValue;
 let displayValue = 0;
 
-screen.textContent = '0';
+screen.textContent = '  ';
 
 numbers.forEach((number) => {
     number.addEventListener('click', (event) => {
@@ -56,9 +64,23 @@ numbers.forEach((number) => {
         displayPressedKeys(+event.target.textContent);
     })
 });
-clear.addEventListener('click', () => { displayValue = 0; screen.textContent = '0'; });
+cl.addEventListener('click', () => { clear(); });
 del.addEventListener('click', () => { updateValues(Math.floor(displayValue / 10)); });
 window.addEventListener('keydown', (e) => {
     if (e.key = 'Backspace') updateValues(Math.floor(displayValue / 10));
 }); //gonna need to to something like this for all keys
+operators.forEach((op) => {
+    op.addEventListener('click', (e) => {
+        lValue = displayValue;
+        displayValue = 0;
+        operator = e.target.textContent;
+        screen.textContent += `${e.target.textContent}`;
+    });
+});
+equals.addEventListener('click', () => {
+    rValue = displayValue;
+    operate(lValue, operator, rValue);
+    screen.textContent = `${operate(lValue, operator, rValue)}`;
+
+})
 
